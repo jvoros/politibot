8c32cbc4af62650b5f127a817253e70611a36e31
Jeremy Voros <jeremyvoros@gmail.com>
2018-01-11 13:51:54 -0700

    pm2 and frontail running

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const Bot = require('./src/Bot');
 const config = require('./config');
 
-const scubbles = new Bot(config.scubblesbot);
-scubbles.streamTweetersTweets(config.rogerrogerbot.id, (tweet) => { 
-  console.log('scubbles received a tweet.');
-  const status = scubbles.extractTweetText(tweet);
-  console.log('status: ', status);
-  const rt = scubbles.manualRetweet(status, 'RT @rogerrogerbot: ');
+const scubbles = new Bot('scubbles', config.scubblesbot);
+scubbles.streamTweetersTweets(config.rogerrogerbot.id, (tweet) => {
+  scubbles.manualRetweet(tweet) 
 });
--- src/Bot.js
+++ src/Bot.js
@@ -1,17 +1,19 @@
 const Twit = require('twit');
 
-function basicCallback(err, data) {
-  if (err) {
-    console.log('Error:'. err);
-    return null;
-  }
-  return data;
-}
-
 class Bot {
-  constructor(botconfig) {
+  constructor(name, botconfig) {
+    this.name = name;
     this.twit = new Twit(botconfig);
-    console.log('Bot initialized....');
+    console.log('STARTING UP:', this.name);
+  }
+
+  tweetCallback(err, data) {
+    if (err) {
+      console.log(`--- ERROR (${this.name})`, err);
+      return null;
+    }
+    console.log(`+++ Status updated (${this.name})`);
+    return data;
   }
 
   tweet(status) {
@@ -20,32 +22,33 @@ class Bot {
     } else if(status.length > 280) {
       return callback(new Error('tweet is too long: ' + status.length));
     }
-    this.twit.post('statuses/update', { status: status }, basicCallback);
+    this.twit.post('statuses/update', { status: status }, this.tweetCallback.bind(this));
   };
 
-  manualRetweet(status, rt_slug) {
-    if (typeof status !== 'string') {
-      return callback(new Error('tweet must be of type String'));
-    } else if (status.length > 280) {
-      return callback(new Error('tweet is too long: ' + status.length));
-    }
-    let new_status;
+  manualRetweet(tweet) {
+    const rt_slug = `RT @${tweet.user.screen_name}: `
+    const status = this.extractStatus(tweet);
     const overage = status.length-(280-rt_slug.length);
+    let new_status;
     if (overage > 0) {
       const trim_position = status.length - overage - 3;
       new_status = `${status.slice(0, trim_position)}...`;
     } else {
       new_status = status;
     }
+    console.log(`::: Retweeting (${this.name}): ${new_status}`);
     return this.tweet(`${rt_slug}${new_status}`);
   }
 
   streamTweetersTweets(ids, callback) {
     const stream = this.twit.stream('statuses/filter', { follow: ids, tweet_mode: 'extended' });
-    stream.on('tweet', (tweet) => callback(tweet));
+    stream.on('tweet', (tweet) => {
+      console.log(`>>> Tweet received (${this.name}): ${tweet.created_at}`);
+      callback(tweet)
+    });
   }
 
-  extractTweetText(tweet) {
+  extractStatus(tweet) {
     return tweet.truncated === true ? tweet.extended_tweet.full_text : tweet.text;
   }
 }
