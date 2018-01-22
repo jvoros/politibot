2127eb21a924f02aabc346078701159d4e3c628b
Jeremy Voros <jeremyvoros@gmail.com>
2018-01-21 23:08:07 -0700

    stop tracking dist files

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var bot_1 = require("./bot");
-var config_1 = require("./config");
-var scubbles = new bot_1.default('scubbles', config_1.default.scubblesbot);
-//# sourceMappingURL=main.js.map
