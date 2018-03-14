fe5a46765a565277559a4a6ca561a9aae845ab68
Jeremy Voros <jeremyvoros@gmail.com>
2018-03-14 09:35:03 -0600

    fix responses

--- src/Library/taxes.ts
+++ src/Library/taxes.ts
@@ -20,9 +20,9 @@ const keywords = [
 ];
 
 const responses = [
-  '@RepMiaLove thinks tax breaks for the ultra-wealthy help real Utahns.',
-  '@RepMiaLove voted for a tax cut for Wall Street.',
-  '@realDonalTrump family got millions in tax breaks. How much did you get?',
+  '.@RepMiaLove thinks tax breaks for the ultra-wealthy help real Utahns.',
+  '.@RepMiaLove voted for a tax cut for Wall Street.',
+  '.@realDonalTrump family got millions in tax breaks. How much did you get?',
   'How does your tax cut compare to your health insurance premium increase?',
 ];
 
