58976f4477c0ae841fb2dbd8c48eafd60df83e7e
Jeremy Voros <jeremyvoros@gmail.com>
2018-03-15 09:32:56 -0600

    git rid of all @s

--- src/Library/healthcare.ts
+++ src/Library/healthcare.ts
@@ -9,6 +9,6 @@ export const healthcare: TopicDef = {
     'prescription',
   ],
   responses: [
-    'Why did $RepMiaLove vote to RAISE the cost of health insurance? http://www.latimes.com/business/hiltzik/la-fi-hiltzik-trumpcare-urban-20180226-story.html'
+    'Why did @RepMiaLove vote to RAISE the cost of health insurance? http://www.latimes.com/business/hiltzik/la-fi-hiltzik-trumpcare-urban-20180226-story.html'
   ],
 }
--- src/Library/immigration.ts
+++ src/Library/immigration.ts
@@ -23,7 +23,7 @@ const keywords = [
 ];
 
 const responses = [
-  'The Statue of Liberty proclaims to the world "Give me your tired, your poor, your huddled masses yearning to breathe free". Why can\'t $RepMiaLove? https://www.nps.gov/stli/learn/historyculture/colossus.htm',
+  'The Statue of Liberty proclaims to the world "Give me your tired, your poor, your huddled masses yearning to breathe free". Why can\'t @RepMiaLove? https://www.nps.gov/stli/learn/historyculture/colossus.htm',
   'https://www.washingtonpost.com/opinions/shithole-wasnt-the-most-offensive-part-of-trumps-haiti-comments/2018/01/12/beba1ea6-f7b3-11e7-a9e3-ab18ce41436a_story.html'
 ];
 
